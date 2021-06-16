961bfe0bb03c2752952f425f900eca39a498d558
seah78 <githubseah.pro>
2021-06-16 18:31:10 +0200

    sauvegarde travail roundcontroller

--- controllers/roundcontroller.py
+++ controllers/roundcontroller.py
@@ -4,7 +4,9 @@
 class RoundController:
 	
 	def __init__(self, current_tournament):
-		self
+		self.tournament = current_tournament
+		self.nb_total_round = self.tournament.number_rounds
+  
 
 	def first_round_controller(self):
 		pass
